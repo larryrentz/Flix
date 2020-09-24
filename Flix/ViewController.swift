a00a7140f778b74932d97d142870b1db7b5ed5e1
Larry Rentz <lrentzjr@ufl.edu>
2020-09-24 00:49:54 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Larry Bernard Rentz Jr on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
