3f937f0d1629552a67783dccd2205e38be931cec
Larry Rentz <lrentzjr@ufl.edu>
2020-10-04 16:01:03 -0400

    Unit 2 Assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Larry Bernard Rentz Jr on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
